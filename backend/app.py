99787e65dcbafa140a11cfb67af051af9f0e4162
Nikitha694 <143313264+Nikitha694@users.noreply.github.com>
2025-10-01 11:52:34 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -126,5 +126,5 @@ def predict():
 
 # ------------------ Run App ------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # dynamic port for Render
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
